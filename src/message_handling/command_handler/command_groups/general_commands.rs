cbb2ece2d1e634ba42da1fe552ade3360d6325ad
Deukhoofd <Deukhoofd@gmail.com>
2021-04-02 16:01:03 +0200

    Updates ping command to be more detailed.

--- src/message_handling/command_handler/command_groups/general_commands.rs
+++ src/message_handling/command_handler/command_groups/general_commands.rs
@@ -1,5 +1,6 @@
 use super::super::command_builder::CommandBuilder;
 use super::super::command_group::CommandGroup;
+use crate::embed::set_default_style;
 use crate::message_handling::command_handler::command_data::CommandData;
 use serenity::builder::{CreateEmbed, CreateMessage};
 use serenity::Error;
@@ -24,7 +25,7 @@ async fn info(req: CommandData) -> Result<(), Error> {
         .channel_id
         .send_message(&req.ctx, |m| {
             m.embed(|e| {
-                crate::embed::set_default_style(e);
+                set_default_style(e);
                 e.title("Deukbot");
                 e.description("A bot designed by Deukhoofd");
                 e.field("Software", "Deukbot 5.0", true);
@@ -39,7 +40,43 @@ async fn info(req: CommandData) -> Result<(), Error> {
 }
 
 async fn ping(req: CommandData) -> Result<(), Error> {
-    info!("PING!");
-    req.message.reply(req.ctx, "Pong!").await?;
+    let t = chrono::Utc::now();
+    let msg_promise = req.message.channel_id.send_message(&req.ctx, |m| {
+        m.embed(|e| {
+            set_default_style(e);
+            e.title("Pong").description("Pong").field(
+                "Measured Ping between Message and Command Handling",
+                format!("{} ms", (t - req.message.timestamp).num_milliseconds()),
+                false,
+            )
+        })
+    });
+    let t2 = chrono::Utc::now();
+    let mut msg = msg_promise.await?;
+    let ts = msg.timestamp;
+    msg.edit(&req.ctx, |m| {
+        m.embed(|e| {
+            set_default_style(e);
+            e.title("Pong")
+                .description("Pong")
+                .field(
+                    "Measured Ping between Message and Command Handling",
+                    format!("{} ms", (t - req.message.timestamp).num_milliseconds()),
+                    false,
+                )
+                .field(
+                    "Measured Time handling embed creation",
+                    format!("{} ms", (t2 - t).num_milliseconds()),
+                    false,
+                )
+                .field(
+                    "Measured Ping between Bot and Discord",
+                    format!("{} ms", (ts - t2).num_milliseconds()),
+                    false,
+                )
+        })
+    })
+    .await?;
+
     Ok(())
 }

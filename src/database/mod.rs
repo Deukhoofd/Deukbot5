a281ff8d8dfdc356a5ff86f919ce50be0b2bf065
Deukhoofd <Deukhoofd@gmail.com>
2021-04-03 12:05:40 +0200

    Reconnect to database if connection is closed.

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -15,12 +15,16 @@ pub async fn get_connection<'a>() -> &'a DatabaseConnection {
         if CONNECTION.is_none() {
             CONNECTION = Some(DatabaseConnection::create().await);
         }
+        if CONNECTION.as_ref().unwrap().client.is_closed() {
+            CONNECTION = Some(DatabaseConnection::create().await);
+        }
         CONNECTION.as_ref().unwrap()
     }
 }
 
 impl DatabaseConnection {
     async fn create() -> DatabaseConnection {
+        info!("Connecting to database");
         let (client, conn) = tokio_postgres::connect(
             env::var("DB_CONNECTION_STRING")
                 .expect("DB Connection String was not set!")
@@ -32,7 +36,7 @@ impl DatabaseConnection {
 
         tokio::spawn(async move {
             if let Err(e) = conn.await {
-                eprintln!("connection error: {}", e);
+                error!("Database connection error: {}", e);
             }
         });
 
